a2d8b7f968f692dba32886efe737b45d3bd39164
Hauke Stieler <mail@hauke-stieler.de>
2018-06-20 20:40:51 +0200

    Rename commands

--- src/picl.go
+++ src/picl.go
@@ -10,14 +10,14 @@ import (
 var (
 	app = kingpin.New("picl", "Maybe the dumbest package manager ever")
 
-	install             = app.Command("install", "Installs the given library")
-	installConfigFile   = install.Flag("config", "Specifies the configuration file that should be used. This is \"./picl.conf\" by default.").Short('c').Default("./picl.conf").File()
-	installOutputFolder = install.Flag("output", "Specifies the output folder where all libraries should be stored. This is \"./libs\" by default.").Short('o').Default("./libs/").File()
-	installUrl          = install.Flag("url", "The base url where picl downloads files from").Short('u').URL()
-	installPackageName  = install.Arg("package", "The library to install").String()
+	installCmd          = app.Command("install", "Installs the given library")
+	installConfigFile   = installCmd.Flag("config", "Specifies the configuration file that should be used. This is \"./picl.conf\" by default.").Short('c').Default("./picl.conf").File()
+	installOutputFolder = installCmd.Flag("output", "Specifies the output folder where all libraries should be stored. This is \"./libs\" by default.").Short('o').Default("./libs/").File()
+	installUrl          = installCmd.Flag("url", "The base url where picl downloads files from").Short('u').URL()
+	installPackageName  = installCmd.Arg("package", "The library to install").String()
 
-	remove            = app.Command("remove", "Uninstalls/removes the given library")
-	removePackageName = remove.Arg("package", "The library to remove").String()
+	removeCmd         = app.Command("remove", "Uninstalls/removes the given library")
+	removePackageName = removeCmd.Arg("package", "The library to remove").String()
 )
 
 func main() {
