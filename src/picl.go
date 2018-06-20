856067491643df70a38291250b7c394eb4b5c367
Hauke Stieler <mail@hauke-stieler.de>
2018-06-21 00:51:06 +0200

    Extract config related code into config.go

--- src/picl.go
+++ src/picl.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"math"
-	"net/url"
 	"os"
-	"regexp"
-	"strings"
 
 	"github.com/hauke96/kingpin"
 	"github.com/hauke96/picl/src/cmd"
@@ -24,91 +19,7 @@ var (
 
 	removeCmd         = app.Command("remove", "Uninstalls/removes the given library")
 	removePackageName = removeCmd.Arg("package", "The library to remove").String()
-
-	configFileCommentRegex   = regexp.MustCompile("^#\\s*\\S*")
-	configFileBlankLineRegex = regexp.MustCompile("^\\s*$")
-	configFileValidRegex     = regexp.MustCompile("^\\s*\\S+\\s*:\\s*\\S+\\s*$")
-
-	configOutputFolder *os.File
-	configRemoteUrl    *url.URL
 )
-
-func readConfig(configFile *os.File) {
-	lines, err := readFile(configFile)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	pairs := make(map[string]string)
-
-	for i, line := range lines {
-		switch {
-		case configFileBlankLineRegex.MatchString(line):
-			continue
-		case configFileCommentRegex.MatchString(line):
-			continue
-		case configFileValidRegex.MatchString(line):
-			splittedLine := strings.SplitN(line, ":", 2)
-
-			if len(splittedLine) != 2 {
-				fmt.Fprintf(os.Stderr, "Parsing line %d failed. This could be an error in the regex, the splitting or the line itself", i)
-				continue
-			}
-
-			key := splittedLine[0]
-			value := splittedLine[1]
-
-			pairs[key] = value
-		default:
-			// To print the first 20 characters of the line, we have to be careful with the bounds of this line
-			upperBound := int(math.Min(float64(len(line)), 20))
-
-			fmt.Fprintf(os.Stderr, "Malformed config-entry in line %d: %s...\n", i, line[:upperBound])
-		}
-	}
-
-	if value, ok := pairs["url"]; ok {
-		urlPtr, err := url.Parse(value)
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error parsing key 'url' from config\n")
-			// TODO further error handling?
-		} else {
-			configRemoteUrl = urlPtr
-		}
-	}
-
-	if value, ok := pairs["output_folder"]; ok {
-		filePtr, err := os.Open(value)
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error parsing key 'output_folder' from config\n")
-			// TODO further error handling?
-		} else {
-			configOutputFolder = filePtr
-		}
-	}
-}
-
-func readFile(file *os.File) ([]string, error) {
-	lines := make([]string, 0)
-
-	// defer closing
-	defer file.Close()
-
-	// read lines
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	// return lines or error
-	if err := scanner.Err(); err != nil {
-		return nil, err
-	}
-
-	return lines, nil
-}
 
 // When a field (e.g. configOutputFolder) is not set yet, but has been specified
 // via a command line argument, it'll be set here. Here we also overwrite
